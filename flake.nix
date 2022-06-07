9a14100154acc2e9800fa1ee16ed712cbaf0383c
Marc 'risson' Schmitt <marc.schmitt@prologin.org>
2022-06-07 22:22:50 +0200

    Initial commit
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@prologin.org>

---
+++ flake.nix
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (c) 2022 Association Prologin <association@prologin.org>
+# Copyright (c) 2022 Marc 'risson' Schmitt <marc.schmitt@prologin.org>
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    futils.url = "github:numtide/flake-utils";
+    renogin.url = "git+https://gitlab.com/prologin/tech/tools/renogin.git";
+  };
+
+  outputs = { self, nixpkgs, futils, renogin } @ inputs:
+    let
+      inherit (nixpkgs) lib;
+      inherit (lib) recursiveUpdate;
+      inherit (futils.lib) eachDefaultSystem defaultSystems;
+
+      nixpkgsFor = lib.genAttrs defaultSystems (system: import nixpkgs {
+        inherit system;
+      });
+    in
+    (eachDefaultSystem (system:
+      let
+        pkgs = nixpkgsFor.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            poetry
+            pre-commit
+            renogin.packages.${system}.default
+          ];
+        };
+      }
+    ));
+}
