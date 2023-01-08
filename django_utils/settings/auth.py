2547d21157e82017275658743b9165d6caa5c922
Marc 'risson' Schmitt <marc.schmitt@prologin.org>
2023-01-08 23:07:41 +0100

    settings: auth: make environment variables generic
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@prologin.org>

--- django_utils/settings/auth.py
+++ django_utils/settings/auth.py
@@ -25,9 +25,9 @@ LOGIN_ERROR_URL = "/"
 # OIDC
 
 OIDC_OP_CONFIG_URL = (
-    env.get_string("PROLOGIN_OIDC_URL") + "/.well-known/openid-configuration"
+    env.get_string("OIDC_OP_CONFIG_URL") + "/.well-known/openid-configuration"
 )
-OIDC_TIMEOUT = env.get_int("PROLOGIN_OIDC_TIMEOUT", 15)
+OIDC_TIMEOUT = env.get_int("OIDC_TIMEOUT", 15)
 
 OIDC_RP_SCOPES = " ".join(["openid", "email", "profile", "roles"])
 OIDC_OP_CONFIG = get_oidc_config(OIDC_OP_CONFIG_URL, timeout=OIDC_TIMEOUT)
@@ -35,8 +35,8 @@ OIDC_OP_AUTHORIZATION_ENDPOINT = OIDC_OP_CONFIG["authorization_endpoint"]
 OIDC_OP_TOKEN_ENDPOINT = OIDC_OP_CONFIG["token_endpoint"]
 OIDC_OP_USER_ENDPOINT = OIDC_OP_CONFIG["userinfo_endpoint"]
 OIDC_OP_LOGOUT_URL_METHOD = OIDC_OP_CONFIG["end_session_endpoint"]
-OIDC_RP_CLIENT_ID = env.get_secret("PROLOGIN_OIDC_CLIENT_ID")
-OIDC_RP_CLIENT_SECRET = env.get_secret("PROLOGIN_OIDC_CLIENT_SECRET")
+OIDC_RP_CLIENT_ID = env.get_secret("OIDC_RP_CLIENT_ID")
+OIDC_RP_CLIENT_SECRET = env.get_secret("OIDC_RP_CLIENT_SECRET")
 OIDC_RP_SIGN_ALGO = env.get_string("OIDC_RP_SIGN_ALGO", "RS256")
 
 if OIDC_RP_SIGN_ALGO == "RS256":
